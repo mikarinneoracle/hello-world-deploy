9f340c8fb358da7a46ffd38e0d821c93dd3794a8
OOW17Demos <32282141+OOW17Demos@users.noreply.github.com>
2017-09-26 16:55:24 -0500

    Create main.go

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/handlers"
+	"net/http"
+	"os"
+)
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello Open World peeps!!!!!"))
+}
+
+func main() {
+	fmt.Printf("Starting Hello World application... \n")
+	http.HandleFunc("/", HelloWorld)
+	http.ListenAndServe(":8080", handlers.LoggingHandler(os.Stdout, http.DefaultServeMux))
+}
+
