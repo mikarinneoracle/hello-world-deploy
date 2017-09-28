acaf62a975b9a1e7f92c6c8e3d567e65a33a60c4
OOW17Demos <32282141+OOW17Demos@users.noreply.github.com>
2017-09-28 13:15:39 -0500

    demo writeup

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello Open World peeps E2E MR rolling test!!!!!!!"))
+	w.Write([]byte("Hello Open World peeps E2E rolling CI/CD demo !!!!!!!"))
 }
 
 func main() {
