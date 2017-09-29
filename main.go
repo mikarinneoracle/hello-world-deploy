b41a363b087d2fdffd42514e6966e7facf4a74cd
OOW17Demos <32282141+OOW17Demos@users.noreply.github.com>
2017-09-29 10:46:07 -0500

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello Open World - This build is for the Video"))
+	w.Write([]byte("Hello Open World - This build is for the Video MR"))
 }
 
 func main() {
