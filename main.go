8f9c71ecd0cd8f83191d777eeb19144bda1dd0c5
OOW17Demos <32282141+OOW17Demos@users.noreply.github.com>
2017-10-02 10:36:07 -0700

    For Mika

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello Open World -  Hello Monday October 2"))
+	w.Write([]byte("Hello Open World -  Hello Monday October 2 demo for Mika"))
 }
 
 func main() {
