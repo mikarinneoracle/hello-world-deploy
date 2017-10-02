834bcf64cad14ee42f18e3609485cfde69b5ca8f
OOW17Demos <32282141+OOW17Demos@users.noreply.github.com>
2017-10-02 16:19:47 -0700

    Happy Hour update

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello Open World -  Hello Juha!! Terve!"))
+	w.Write([]byte("Hello Open World -  Hello Happy Hour"))
 }
 
 func main() {
