0b72dbb06ec4a9573d6c1aa573d06ceece5471aa
OOW17Demos <32282141+OOW17Demos@users.noreply.github.com>
2017-10-03 10:47:44 -0700

    amdocs

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello Open World -  Avi"))
+	w.Write([]byte("Hello Open World -  amdocs"))
 }
 
 func main() {
