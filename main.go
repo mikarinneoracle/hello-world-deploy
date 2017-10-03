1a4bab6e29200c4f685cbf1bb8fe884bf6594f63
OOW17Demos <32282141+OOW17Demos@users.noreply.github.com>
2017-10-03 09:59:36 -0700

    build for Avi

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello Open World -  sita"))
+	w.Write([]byte("Hello Open World -  Avi"))
 }
 
 func main() {
