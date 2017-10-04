6e3137773e7c2f30f7fc8afc1d6f705ab1a65234
OOW17Demos <32282141+OOW17Demos@users.noreply.github.com>
2017-10-04 11:54:44 -0700

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello Open World -  Hello Alan"))
+	w.Write([]byte("Hello Open World -  Hello Jon"))
 }
 
 func main() {
